e261a41f1bb6147c95551b0d565fb8d74a14c25c
Sammy Chien <sammychien@utexas.edu>
2019-06-10 21:59:04 -0500

    Partially Complete

--- src/wordhunt/WordHunt.java
+++ src/wordhunt/WordHunt.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.SortedSet;
@@ -18,11 +19,10 @@ public class WordHunt {
 	private static BufferedReader reader;
 	private static BufferedWriter writer;
 	
-	private static SortedSet<String> masterWordList;
-
 	public static void main(String[] args) {
 		// System.out.println(readInput());
 		reader = null; writer = null;
+		SortedSet<String> masterWordList = new TreeSet<String>();
 		try {
 			reader = FileCreation.initializeBR("FilteredWords.txt");
 			writer = FileCreation.initializeBW("WordHuntSoln.txt");
@@ -43,8 +43,7 @@ public class WordHunt {
 		 * if there are no more adjacent tiles, backtrack (simply return)
 		 * 
 		 */
-		ArrayList<String> results = new ArrayList<String>();
-		results = solve(input, masterWordList);
+		Set<String> results = solve(input, masterWordList);
 		
 		System.out.println(results);
 
@@ -75,8 +74,8 @@ public class WordHunt {
 		return set;
 	}
 	
-	private static ArrayList<String> solve(ArrayList<Character> inputChar, SortedSet<String> dictionary) {
-		ArrayList<String> results = new ArrayList<String>();
+	private static Set<String> solve(ArrayList<Character> inputChar, SortedSet<String> dictionary) {
+		Set<String> results = new HashSet<String>();
 		// instantiate the grid
 		Grid grid = new Grid(inputChar);
 		// grid has Map<Coordinate, Tile> 
@@ -86,7 +85,7 @@ public class WordHunt {
 		for (int i = 0; i < grid.tiles.size(); i++) {
 			// start with this particular tile
 			Tile t = grid.tiles.get(i); 
-			results.addAll(solveBFS(t, grid));
+			results.addAll(solveBFS(t, grid, dictionary));
 		}
 		return results;
 	}
@@ -95,14 +94,34 @@ public class WordHunt {
 	 *  Want to pass in current tile and grid
 	 *  don't forget about removing all breadcrumbs before exiting!
 	 */
-	private static ArrayList<String> solveBFS(Tile currentTile, Grid grid) {
+	private static ArrayList<String> solveBFS(Tile currentTile, Grid grid, SortedSet<String> dictionary) {
 		ArrayList<String> list = new ArrayList<String>();
 		
 		// make prefix
 		String prefix = "";
 		prefix += currentTile.getC();
 		
-		// testing git branchess
+		// Let's begin to do this BFS stuff
+		
+		/*
+		 * TreeSet is already a sorted set
+		 * However, I need a way to check whether a prefix of a word exists inside the treeset with a O(log n)
+		 * This is difficult since the TreeSet only checks against the entire word. 
+		 * For example, if my prefix were walle, the dictionary doesn't have walle and it would give back false
+		 * However, I could still get wallet, so I want the function to give me a true
+		 * Basically, I want the search function to also return fragments
+		 * 
+		 * Fuck it
+		 * make this not very optimized
+		 * check when word is between 3-8 characters long
+		 * 
+		 * 
+		 */
+		
+		
+		
+		
+		
 		list.add(prefix);
 		
 		
@@ -110,7 +129,36 @@ public class WordHunt {
 	}
 	
 	
+	private static ArrayList<String> solveBFS(Tile currentTile, Grid grid, SortedSet<String> dictionary, 
+			String prefix, ArrayList<String> resultList) { 
+		
+		// if prefix is 8 letters long, return
+		if (prefix.length() >= Params.maxWordLen) return resultList;
+		
+		// if prefix matches a word in the dictionary, 
+		if (isPrefixInDict(dictionary, prefix)) {
+			resultList.add(prefix);
+		} // keep going since there might be more words
+		
+		// now check all surrounding tiles
+		
+		
+		
+		
+		return null;
+	}
 	
+	/*
+	 * determine whether or not the prefix is in the dictionary
+	 * NOT VERY OPTIMIZED RIGHT NOW
+	 * TODO: Make O(log n) algorithm
+	 */
+	private static boolean isPrefixInDict(SortedSet<String> dictionary, String prefix) {
+		for (String s: dictionary) {
+			if (prefix.equals(s)) return true;
+		}
+		return false;
+	}
 	
 	
 	
