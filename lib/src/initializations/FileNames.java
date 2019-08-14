260e4363d8e1b0855ab187b768c3d8298e28d6f3
Sammy Chien <sammychien@utexas.edu>
2019-08-14 17:06:26 -0500

    Used path to get directory path changed name of project to WordGames

--- lib/src/initializations/FileNames.java
+++ lib/src/initializations/FileNames.java
@@ -1,14 +1,16 @@
 package initializations;
 
+import java.nio.file.Paths;
+
 /**
- * <h1>FileNames</h1>
- * The FileNames class defines file paths for methods
+ * <h1>FileNames</h1> The FileNames class defines file paths for methods
  * 
  * @author sammychien
  *
  */
 public class FileNames {
-    public final static String fileDirectory = "WordGames/lib/files";
+    public final static String rootDirectory = Paths.get("").toAbsolutePath().toString();
+    public final static String fileDirectory = rootDirectory + "/lib/files";
     public final static String dictionaryDirectory = fileDirectory + "/Dictionaries";
     public final static String solutionsDirectory = fileDirectory + "/Solutions";
     public final static String xLetterWords = fileDirectory + "/XLetterWords";
